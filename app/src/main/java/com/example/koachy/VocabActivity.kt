130499e7f867481fff202b4be3b4821de930ae08
Chanshirecat <happiness.delight@outlook.com>
2023-02-20 22:19:51 +0100

    made Activity- and Fragment-structure

--- app/src/main/java/com/example/koachy/VocabActivity.kt
+++ app/src/main/java/com/example/koachy/VocabActivity.kt
@@ -3,9 +3,9 @@ package com.example.koachy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class VocabActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_vocab)
     }
 }
