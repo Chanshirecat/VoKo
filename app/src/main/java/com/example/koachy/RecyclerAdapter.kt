d3ef06e5b2da403aaca03bfb6694e2c3e5e592e2
Chanshirecat <happiness.delight@outlook.com>
2023-02-21 00:17:04 +0100

    added recyclerview

---
+++ app/src/main/java/com/example/koachy/RecyclerAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.koachy
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private var koreanVocab = arrayOf("Word One", "Word Two", "Word Three", "Word Four", "Word Five", "Word Six", "Word Seven")
+    private var englishVocab = arrayOf("Translation One", "Translation Two", "Translation Three", "Translation Four", "Translation Five", "Translation Six", "Translation Seven")
+    private var koreanFlag = intArrayOf(R.drawable.koreawipfel,R.drawable.koreawipfel,R.drawable.koreawipfel,R.drawable.koreawipfel,R.drawable.koreawipfel,R.drawable.koreawipfel,R.drawable.koreawipfel)
+        private var englishFlag = intArrayOf(R.drawable.ukwipfel,R.drawable.ukwipfel,R.drawable.ukwipfel,R.drawable.ukwipfel,R.drawable.ukwipfel,R.drawable.ukwipfel,R.drawable.ukwipfel)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+    val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+    return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return koreanFlag.size
+        return koreanVocab.size
+        return englishFlag.size
+        return englishVocab.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+    holder.koreanVocab.text = koreanVocab[position]
+    holder.englishVocab.text = englishVocab[position]
+    holder.koreanFlag.setImageResource(koreanFlag[position])
+        holder.englishFlag.setImageResource(englishFlag[position])
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    var koreanFlag: ImageView
+    var koreanVocab: TextView
+    var englishFlag: ImageView
+    var englishVocab: TextView
+
+    init {
+        koreanFlag = koreanFlag.findViewById(R.id.koreanFlag)
+        koreanVocab = koreanVocab.findViewById(R.id.koreanVocab)
+        englishFlag = englishFlag.findViewById(R.id.englishFlag)
+        englishVocab = englishVocab.findViewById(R.id.englishVocab)
+        }
+    }
+}
--- app/src/main/java/com/example/koachy/VocabActivity.kt
+++ app/src/main/java/com/example/koachy/VocabActivity.kt
@@ -2,10 +2,24 @@ package com.example.koachy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class VocabActivity : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vocab)
+
+        layoutManager = LinearLayoutManager(this)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = RecyclerAdapter()
+        recyclerView.adapter = adapter
     }
 }
